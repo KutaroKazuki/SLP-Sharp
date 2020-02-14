aa8db2677f5793762386671570bc416e486d3c68
KutaroKazuki <58274633+KutaroKazuki@users.noreply.github.com>
2020-02-14 23:57:45 +0100

    Update CmdOptions.cs
    i modified the ip to listen to 0.0.0.0 instead of 127.0.0.1 wich was only listening on the machine running the Code/Server

--- SwitchLanNet/Helpers/CmdOptions.cs
+++ SwitchLanNet/Helpers/CmdOptions.cs
@@ -9,7 +9,7 @@ namespace SwitchLanNet.Helpers
 {
     public class CmdOptions
     {
-        [Option("ip", Required = false, HelpText = "IP to listen on, defaults to 127.0.0.1(localhost).", Default = "127.0.0.1")]
+        [Option("ip", Required = false, HelpText = "IP to listen on, default was 127.0.0.1(localhost).", Default = "0.0.0.0")]
         public string Address { get; set; }
 
         [Option('p', "port", Required = false, Default = 11451, HelpText = "Port to listen on(default 11451)")]
